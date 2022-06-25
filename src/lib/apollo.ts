19313285c0a3eebb62316efc4d2a5253f05981cf
Carla Bruckmann <carlaabruckmann@gmail.com>
2022-06-24 21:34:26 -0300

    feat(app) added token API and update uri

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o90hy71cgi01xmc79eg363/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Autorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
