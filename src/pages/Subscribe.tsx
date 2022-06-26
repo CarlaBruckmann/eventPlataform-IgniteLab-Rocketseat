7ebcdc627b5fa42d9e2c97b2c624341526d1be08
Carla Bruckmann <carlaabruckmann@gmail.com>
2022-06-25 22:53:54 -0300

    refactor(app) update image mockup

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,8 +1,8 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import ImageMockup from "../assets/code-mockup.png";
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -74,7 +74,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-mockup.png" className="mt-10 px-5" alt="" />
+      <img src={ImageMockup} alt="Mockup" className="mt-10 px-5" />
     </div>
   );
 }
